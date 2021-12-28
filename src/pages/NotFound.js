2b9bb69bbcc56103dc781f6011127d1f7fab048f
Alemkhan <alemkhan.yergaliev@gmail.com>
2021-12-28 22:23:31 +0600

    React router practice task

---
+++ src/pages/NotFound.js
@@ -0,0 +1,5 @@
+const NotFound = () => {
+  return <p>Page Not Found!</p>;
+};
+
+export default NotFound;
---
+++ src/pages/QuoteDetail.js
@@ -0,0 +1,49 @@
+import { useParams, Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import Comments from "../components/comments/Comments";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
+import LoadingSpinner from "../components/UI/LoadingSpinner";
+import useHttp from "../hooks/use-http";
+import { getSingleQuote } from "../lib/api";
+
+const QuoteDetail = () => {
+  const params = useParams();
+
+  const { quoteId } = params;
+
+  const {
+    sendRequest,
+    status,
+    data: loadedQuote,
+    error,
+  } = useHttp(getSingleQuote, true);
+
+  useEffect(() => {
+    sendRequest(quoteId);
+  }, [sendRequest, quoteId]);
+
+  if (status === "pending") {
+    return (
+      <div className="centered">
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if (status === "error") {
+    return <div className="centered focused">{error}</div>;
+  }
+
+  if (!loadedQuote.text) {
+    return <p>No quote found</p>;
+  }
+
+  return (
+    <>
+      <HighlightedQuote text={loadedQuote.text} author={loadedQuote.author} />
+      <Outlet />
+    </>
+  );
+};
+
+export default QuoteDetail;
